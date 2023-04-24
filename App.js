f92e0872e14c56f426f5ae7118024dd1fad35bcf
Andy Bartkiv <andybartkiv@AccedoAdmins-MacBook-Pro.local>
2023-04-24 18:06:10 -0400

    add more style

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { ActivityIndicator, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [items, setItems] = useState(['Hello Andy']);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ActivityIndicator size={'large'} />
+      <TouchableOpacity>
+        <Text style ={styles.text}>HELLO Andy!</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 20,
   },
+  text: {
+    backgroundColor: '#234',
+    padding: 10,
+    borderRadius: 10,
+    color: 'orange',
+  }
 });
